00394660b1665d7c8e06b7c58fdd3ace25f6b1c0
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-03-11 14:49:21 +0000

    add basic functions and config

---
+++ R/functions/log_info.R
@@ -0,0 +1,14 @@
+log_info <- function(
+    message = "",
+    verbose = getOption("log_verbose")) {
+  if (verbose == TRUE) {
+    cat(
+      paste("[INFO][",
+            substr(Sys.time(), 12, 19),
+            "]: ",
+            message, "\n",
+            sep = ""
+      )
+    )
+  }
+}
---
+++ R/scripting/config.R
@@ -0,0 +1,4 @@
+source("R/functions/source_functions.R")
+source_functions()
+
+options(log_verbose = TRUE)
