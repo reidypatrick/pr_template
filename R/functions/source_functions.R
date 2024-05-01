8c12b34e8e0ab99eb9bcf395115746ded1cadee5
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-05-01 16:57:23 +0100

    fix lints

--- R/functions/source_functions.R
+++ R/functions/source_functions.R
@@ -2,16 +2,16 @@ source_functions <- function(py = FALSE) {
   # Source R functions
   r_functions_path <- file.path(getOption("r_functions_path"))
   r_functions <- list.files(r_functions_path)
-  
+
   for (f in seq_along(r_functions)) {
     source(file.path(r_functions_path, r_functions[f]), verbose = FALSE)
   }
-  
+
   if (py) {
     # Source python functions
     py_functions_path <- file.path("python/functions")
     py_functions <- list.files(py_functions_path)
-    
+
     for (f in seq_along(py_functions)) {
       reticulate::source_python(file.path(py_functions_path, py_functions[f]))
     }
