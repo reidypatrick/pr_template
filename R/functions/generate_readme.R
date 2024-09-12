c059c72cadc6d8a26a04354281ddbc89835ddc81
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2024-09-12 13:38:25 +0100

    create dynamic readme generation

---
+++ R/functions/generate_readme.R
@@ -0,0 +1,32 @@
+# Function to create a basic README file
+create_readme <- function(
+  file_name = "README.md",
+  proj_name = sub(".*/([^/]+)$", "\\1", rstudioapi::getActiveProject()),
+  description = "Project description goes here."
+) {
+  
+  # Define the content of the README
+  lintr_badge_yml <- paste0(
+    "[![lint-project](https://github.com/reidypatrick/",
+    proj_name,
+    "/actions/workflows/lint-project.yaml/badge.svg)](https://github.com/reidypatrick/",
+    proj_name,
+    "/actions/workflows/lint-project.yaml)"
+  )
+  
+  content <- c(
+    lintr_badge_yml,
+    "",
+    
+    paste0("# ", proj_name),
+    
+    "",
+    "## Description",
+    description
+  )
+
+  # Write content to the README.md file
+  writeLines(content, con = file_name)
+
+  cat("README file created successfully: ", file_name, "\n")
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 [![lint-project](https://github.com/reidypatrick/pr_template/actions/workflows/lint-project.yaml/badge.svg)](https://github.com/reidypatrick/pr_template/actions/workflows/lint-project.yaml)
 
 # pr_template
-Template for R projects
+
+## Description
+Project description goes here.
